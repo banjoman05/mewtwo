a204d5c05adc38abda8ff5238cb9358fde2e5004
banjoman05 <kory.miller@gmail.com>
2019-01-20 22:57:06 -0500

    Update start_mewtwo.sh

--- start_mewtwo.sh
+++ start_mewtwo.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This is the string we will check via the ps command below.
+# Change "/home/mewtwo/mewtwo.js" to match your env.
 SERVICE="/usr/bin/node /home/mewtwo/mewtwo.js"
 
 var1=`ps -ef | grep -v grep | grep "$SERVICE" | awk '{print $2}'`
@@ -12,7 +14,7 @@ if [ -n "$var1" ]
 else
  echo "Process not found"
 
- # Clean up
+ # Clean up old screen session
  screen -ls | grep mewtwo | cut -d. -f1 | awk '{print $1}' | xargs kill
 
  # Restart
